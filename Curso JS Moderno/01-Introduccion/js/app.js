d55ea728c8ee3da2f510e18ef67ee022893f3c42
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-03-06 10:40:18 -0600

    leccion 1 terminada

---
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,23 @@
+//Toma el nombre y despues lo muestra en pantalla
+const nombre = prompt("Cual es tu nombre");
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JS moderno`;
+
+/*
+Diferentes tipos de console
+
+console.log('log normal')
+console.table('muestra datos como una tabla')
+console.error('muestra errores')
+console.warn('Muestra advertencias')
+console.clear();
+console.time('hola');
+console-timeEnd('Hola');
+
+*/
+
+
+console.log('hola');
+console.log('eslint');
+
+
+
