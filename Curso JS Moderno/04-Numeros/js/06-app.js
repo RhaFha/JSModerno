6ba166654c6187a0c76e1535d9bdd692645db178
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-03-06 16:53:45 -0600

    Leccion 5 operadores terminada

--- Curso JS Moderno/04-Numeros/js/06-app.js
+++ Curso JS Moderno/04-Numeros/js/06-app.js
@@ -5,3 +5,8 @@ const numero4 = 20;
 
 console.log(Number.parseInt(numero1));
 console.log(Number.parseFloat(numero2));
+
+
+//Revisar si un numero es entero o no
+console.log(Number.isInteger(number1));
+
---
+++ Curso JS Moderno/05-Operadores/js/03-app.js
@@ -0,0 +1,12 @@
+let numero1;
+
+console.log(numero1);
+console.log(typeof numero1);
+
+
+//null
+let numero2 = null;
+console.log(numero2);
+console.log(typeof numero2);
+
+console.log(numero1 === numero2);
