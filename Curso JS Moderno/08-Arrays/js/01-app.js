7f2110c03ca99503d487cac5273b0a76a0978ac2
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-03-07 11:43:08 +0700

    Leccion 8 Arrays incompleta

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+console.log(numeros);
+console.log(meses);
+
+//Una arreglo con datos de todo tipo
+
+const deTodo = [ 'Hola', 10, true, "si", null, {nombre: 'Juan', trabajo: 'Programador'}];
+console.log(deTodo);
