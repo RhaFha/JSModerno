00bb79e4262170222b692bcca043c55b8f3c267a
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-03-07 10:14:02 +0700

    Leccion 6 Boolenas terminada

---
+++ Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,14 @@
+const autenticado = true;
+
+/*
+if (autenticado) {
+    console.log('Si puedes ver netflix');
+} else {
+    console.log('No o puedes verlos');
+}
+
+*/
+
+
+//Operador ternario
+console.log( autenticado ? 'Si esta autenticado' : 'No esta autenticado');
